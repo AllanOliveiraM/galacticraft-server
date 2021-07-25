835eccf47be21d39c7b57759506b40f69c56ebde
Allan Oliveira Miraballes <allan.miraballes@nave.rs>
2021-07-25 03:49:07 -0300

    auto

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-java -Xmx4096M -Xms4096M -jar forge-1.12.2-14.23.5.2854.jar -o true
+git add .
+git commit -m "auto"
+git push origin main
+
+# java -Xmx4096M -Xms4096M -jar forge-1.12.2-14.23.5.2854.jar -o true
 
