bb3c660ef1460a9478b9ec610a1a56f923b94432
Allan Oliveira <nallamo2016@gmail.com>
2023-09-30 17:49:55 -0300

    init new forge

--- run.bat
+++
@@ -1,2 +0,0 @@
-java -Xmx6144M -Xms6144M -jar forge-1.12.2-14.23.5.2854.jar -o true
-pause
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ git add .
 git commit -m "auto"
 git push origin main
 
-java -Xmx4096M -Xms4096M -jar forge-1.12.2-14.23.5.2854.jar -o true
+/home/allan/Documentos/jre1.8.0_381/bin/java -Xmx4096M -Xms4096M -jar forge-1.12.2-14.23.5.2859.jar -o true
 
